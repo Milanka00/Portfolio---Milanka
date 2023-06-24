7b4a4751949e727f155ac04535e1139bf85e1830
Milanka00 <milankatharangana01@gmail.com>
2023-06-24 16:42:53 +0530

    tool set

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -4,8 +4,9 @@ import {
   SiLinux,
   SiVisualstudiocode,
   SiPostman,
+  SiIntellijidea,
   
-  SiVercel,
+
 
   SiGithub,
 } from "react-icons/si";
@@ -14,16 +15,13 @@ function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiLinux />
+        <SiIntellijidea />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiGithub />
