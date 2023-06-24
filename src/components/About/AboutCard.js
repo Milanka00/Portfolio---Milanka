5431e3e38716144b98d93b96fc5d36262d51b8e0
Milanka00 <milankatharangana01@gmail.com>
2023-06-24 16:24:12 +0530

    color

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import "./about.css";
 
 function AboutCard() {
   return (
     <Card className="quote-card-view ">
       <Card.Body>
         <blockquote className="blockquote mb-0" data-aos="zoom-in">
-          <p style={{ textAlign: "justify" }} className="home-about-body">
+          <p style={{ textAlign: "justify" }} >
             Hi Everyone, I am <span className="primary-header">Milanka Tharangana</span> from Galle, Sri Lanka. I am an IT undergraduate from university of Moratuwa and a passionate programmer.
             <br />
             <br />
